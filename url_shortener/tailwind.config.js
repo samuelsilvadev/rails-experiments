7e308ed8adf148504279ab5c36f88e9da0ced293
Samuel Silva <samuelsilvawb@gmail.com>
2025-03-08 16:19:18 +0000

    chore(url_shortener): setup tailwind

---
+++ url_shortener/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./app/views/**/*.html.erb",
+    "./app/layouts/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/controllers/**/*.rb",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
