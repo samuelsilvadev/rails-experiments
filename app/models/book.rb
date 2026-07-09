2c4533d6353619ad61833a941b8c5a5a5aff1fbf
Samuel Silva <samuelsilvawb@gmail.com>
2026-07-09 21:20:29 +0100

    chore: annotate all models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  author_id  :integer          not null
+#
+# Indexes
+#
+#  index_books_on_author_id  (author_id)
+#
+# Foreign Keys
+#
+#  author_id  (author_id => authors.id)
+#
 class Book < ApplicationRecord
   belongs_to :author
 end
--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -1,5 +1,14 @@
 require "test_helper"
 
+# == Schema Information
+#
+# Table name: authors
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class AuthorTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
